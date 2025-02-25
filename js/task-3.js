0bff02ceb60422012802ed44da81a1ba4cccfc96
alex_matviyuk <alexmatviyuk1@gmail.com>
2025-02-25 22:32:36 +0200

    homework 8

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-  const trimmedValue = event.currentTarget.value.trim();
-
-  output.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
-});
